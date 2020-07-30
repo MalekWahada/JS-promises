e294dad611694da08524f5c85adb1f60b2f230fb
Malek <malekcheck@gmail.com>
2020-07-30 15:34:05 +0100

    async + promise all

--- promises.js
+++ promises.js
@@ -27,7 +27,41 @@ function createPost(post) {
     })
 }
 
+/////////////////// single promise
+// createPost({title: 'post 3', body: 'this post three'})
+// .then(getPosts)
+//     .catch(err => console.log(err));
 
-createPost({title: 'post 3', body: 'this post three'})
-.then(getPosts)
-    .catch(err => console.log(err));
+
+/////////////////// group of promises
+// Promise.all
+
+// const promise1 = Promise.resolve('Hello world');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => 
+// setTimeout(resolve, 2000, 'Good bye'));
+
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json());
+
+// Promise.all([promise1, promise2, promise3, promise4]).then(
+//     (values) => console.log(values)
+// );
+
+
+/////////////////// Async await
+// async function init(){
+//     await createPost({title: 'post 3', body: 'this post three'});
+//     getPosts();
+// }
+
+// init();
+
+/////////////////// Async await with fetch
+async function fecthUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    console.log(data);
+}
+
+fecthUsers();
